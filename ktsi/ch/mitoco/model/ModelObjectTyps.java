4ed1178667c0116d69ed7a57da73e4bda884ba0a
sandrich <steffe21@gmail.com>
2011-05-29 20:56:20 +0200

    DataHandling ObjectTypen created

---
+++ ktsi/ch/mitoco/model/ModelObjectTyps.java
@@ -0,0 +1,22 @@
+package ch.mitoco.model;
+
+import java.util.List;
+
+public class ModelObjectTyps {
+	private List<ModelMtObjects> objecttyp;
+
+	/**
+	 * @param objecttyp the objecttyp to set
+	 */
+	public void setObjecttyp(List<ModelMtObjects> objecttyp) {
+		this.objecttyp = objecttyp;
+	}
+
+	/**
+	 * @return the objecttyp
+	 */
+	public List<ModelMtObjects> getObjecttyp() {
+		return objecttyp;
+	}
+
+}
